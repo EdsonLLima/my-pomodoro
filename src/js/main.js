4b9630553a00ed968a7d5db9e6348a221930c42f
Luiz Lima <edneto.eternamente@gmail.com>
2023-10-06 23:47:31 -0300

    add displau none and add setItem localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 const action = document.querySelector('#action')
-const stop = document.querySelector('#stop')
+const stop_session = document.querySelector('#stop')
 const sessions = document.querySelector('#sessions')
 const seconds = 0
 
@@ -11,19 +11,27 @@ const lofi = document.querySelector('#lofi')
 const play = document.querySelector('#play')
 const pause = document.querySelector('#pause')
 
+
 const start = () => {
     if (action.value == 0) {
         document.querySelector('#erro_action').innerHTML = "adicione os minutos"
         action.focus()
-    } else if (stop.value == 0) {
+    } else if (stop_session.value == 0) {
         document.querySelector('#erro_stop').innerHTML = "adicione a  pausa"
-        stop.focus()
+        stop_session.focus()
     } else if (sessions.value == 0) {
         document.querySelector('#erro_sessions').innerHTML = "adicione as sessões  "
         sessions.focus()
     } else {
         lofi.play()
         pause.style.setProperty('display', 'block', 'important')
+
+        localStorage.setItem('action', String(action.value))
+        localStorage.setItem('stop', String(stop_session.value))
+        localStorage.setItem('sessions', String(sessions.value))
+
+        document.getElementById('config').style.setProperty('display', 'none', 'important')
+        document.getElementById('timer').style.setProperty('display', 'block', 'important')
 
     }
 
